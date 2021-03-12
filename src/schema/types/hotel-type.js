87dfcf3425a2f64cef64c0287fd4701293434c11
Randi Mikkelsen Bech <au615785@uni.au.dk>
2021-03-12 12:01:37 +0100

    Now with saved files

--- src/schema/types/hotel-type.js
+++ src/schema/types/hotel-type.js
@@ -3,25 +3,44 @@ import {
     GraphQLString,
     GraphQLList,
     GraphQLNonNull,
-    GraphQLNumber,
+    GraphQLInt,
+    GraphQLID,
 } from 'graphql';
 
 const HotelType = new GraphQLObjectType({
     name: 'Hotel',
     description: 'Hotel schema',
     fields: {
-        id: {type: new GraphQLNonNull(GraphQLNumber)},
+        id: {type: new GraphQLNonNull(GraphQLID)},
         name: {type: new GraphQLNonNull(GraphQLString)},
-        managerId: {type: new GraphQLNonNull(GraphQLString)},
-        rooms: {type: new GraphQLList(RoomType)}
+        managerId: {type: new GraphQLNonNull(GraphQLID)},
+        rooms: {type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(RoomType)))}
     }
 })
 
 const RoomType = new GraphQLObjectType({
-    name: 'Rooms',
-    description: 'Rooms in hotel',
+    name: 'Room',
+    description: 'Room in hotel',
     fields: {
-        id: {type: new GraphQLNonNull(GraphQLNumber)},
-        roomNo: {type: new GraphQLNonNull(GraphQLNumber)},
+        id: {type: new GraphQLNonNull(GraphQLID)},
+        roomNo: {type: new GraphQLNonNull(GraphQLInt)},
+        reservations: {type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(ReservationType)))}
+    }
+})
+
+const ReservationType = new GraphQLObjectType({
+    name: 'Reservation',
+    description: 'Reservation in Room',
+    fields: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        guestId: { type: new GraphQLNonNull(GraphQLID) },
+        dateStart: {
+            type: new GraphQLNonNull(GraphQLString),
+            resolve: (source) => source.dateStart.toISOString(),
+        },
+        dateEnd: {
+            type: new GraphQLNonNull(GraphQLString),
+            resolve: (source) => source.dateEnd.toISOString(),
+        }
     }
 })
